ac240dd730ddc5a2f98cdf5fd79619a848b650b8
AnasTrk <anastarak9@gmail.com>
2022-03-10 19:29:04 +0100

    State :: To :: Validate

--- get_ips_v3.sh
+++ get_ips_v3.sh
@@ -2,18 +2,7 @@
 
 
 
-#########################################################################################################
-#                                       USAGE                                                           #
-#   command :: get_ips_v3.sh [-e (env) <string>] [-s (save) <string>] [-c (cluster) <string>]           #
-#                                                                                                       #
-#                                                                                                       #
-#   [-e] :: this flag allows you sepcify the environement you're searching for .                        #
-#   [-s] :: this flag allows you save the output of command into a file (Not working yet just ignore)   #
-#   [-c] :: you can specify the cluster name that the environement belongs to .                         #
-#                                                                                                       #
-#   the aws cli command used in this script is aws describe-instances                                   #
-#   (Not like the one in get_ips_v2.py)                                                                 #
-#########################################################################################################
+
 usage() { echo "Usage: $0 [-e (env) <string>] [-s (save) <string>] [-c (cluster) <string>]" 1>&2; exit 1; }
 
 
@@ -31,9 +20,23 @@ do
     esac
 done
 
-aws ec2 describe-instances \
-    --region $REGION 
-    --filters   Name=tag-key,Values=Name \
-                Name=key-name,Values=$cluster \
+
+if [ ! "$env" ]; then
+    aws ec2 describe-instances \
+    --region eu-west-1 \
+    --filters \
+    "Name=tag-key,Values=Name" \
+    "Name=key-name,Values=$cluster" \
     --query 'Reservations[*].Instances[*].{Instance:InstanceId,AZ:Placement.AvailabilityZone,Name:Tags[?Key==`Name`]|[0].Value,PrivateIpAddress:PrivateIpAddress}' \
-    --output table 
+    --output table
+else
+    aws ec2 describe-instances \
+    --region eu-west-1 \
+    --filters \
+    "Name=tag-key,Values=Name" \
+    "Name=tag-value,Values=$env" \
+    "Name=key-name,Values=$cluster" \
+    --query 'Reservations[*].Instances[*].{Instance:InstanceId,AZ:Placement.AvailabilityZone,Name:Tags[?Key==`Name`]|[0].Value,PrivateIpAddress:PrivateIpAddress}' \
+    --output table
+fi
+
